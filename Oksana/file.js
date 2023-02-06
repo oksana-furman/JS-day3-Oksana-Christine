9dcbf54975296bcfcbd42e41eb334382e1b82568
oksana-furman <oksan9a.furman@gmail.com>
2023-02-06 14:51:39 +0100

    add intermediate exxercises

--- Oksana/file.js
+++ Oksana/file.js
@@ -31,3 +31,58 @@ if (temp < -5) {
 } else {
     document.getElementsByClassName("headline")[1].innerHTML = "The weather is too hot";
 }
+
+// intermediate exercise 1 - Compare two numbers
+
+let students = ["Martin", "Thomas", "Klaus", "Maria", "David"];
+let points = [76, 85, 65, 93, 81];
+let sum = 0;
+let avr = 0;
+for (let i = 0; i < points.length; i++) {
+    sum += points[i];
+    avr = sum / points.length;
+    console.log(sum, avr);
+}
+
+if (avr < 60) {
+    classGrade = "F";
+} else if (avr < 70) {
+    classGrade = "D";
+} else if (avr < 80) {
+    classGrade = "C";
+} else if (avr < 90) {
+    classGrade = "B";
+} else if (avr <= 100) {
+    classGrade = "A";
+}
+document.getElementsByTagName("p")[0].innerHTML = "Class result is " + avr + ", the avarage grade is " + classGrade + "<br>";
+
+for (var i = 0; i < points.length; i++) {
+    if (points[i] < 60) {
+        grade = "F";
+    } else if (points[i] < 70) {
+        grade = "D";
+    } else if (points[i] < 80) {
+        grade = "C";
+    } else if (points[i] < 90) {
+        grade = "B";
+    } else if (points[i] <= 100) {
+        grade = "A";
+    }
+    document.getElementsByTagName("p")[0].innerHTML += `The result of ${students[i]} is ${points[i]}, their avarage grade is  ${grade}. <br>`;
+}
+
+
+// intermediate exercise 2 - FizzBuzz
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        document.getElementById("p3").innerHTML += "FizzBuzz" + "<br>";
+    } else if (i % 3 == 0) {
+        document.getElementById("p3").innerHTML += "Fizz" + "<br>";
+    } else if (i % 5 == 0) {
+        document.getElementById("p3").innerHTML += "Buzz" + "<br>";
+    } else {
+        document.getElementById("p3").innerHTML += i + "<br>";
+    }
+}
