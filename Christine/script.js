916847ed45630713fbdd244f51a0b3f7c0d77a2c
ChristineWeidhofer <xtine@gmx.at>
2023-02-06 12:59:09 +0100

    intermediate 2

--- Christine/script.js
+++ Christine/script.js
@@ -13,19 +13,20 @@ console.log(max);
 
 //Temperature
 
-let temp = Math.floor(Math.random() * (45-(-10)) + (-10)); // Math.random() * (max_number-min_number) + min_number;
+let temp = Math.floor(Math.random() * (45 - (-10)) + (-10)); // Math.random() * (max_number-min_number) + min_number;
 console.log(temp);
 
+let selTemp = document.getElementById("temp");
 
 if (temp <= 10) {
-  document.getElementById("temp").innerHTML = (`<p>The weather is cold, because it has ${temp} degrees Celsius.</p></br><img src="winter.jpg" alt="cold">`);
+  selTemp.innerHTML = (`<p>The weather is cold, because it has ${temp} degrees Celsius.</p></br><img src="winter.jpg" alt="cold">`);
   document.getElementById("temp").style.backgroundColor = "blue";
 } else if (temp <= 28) {
-  document.getElementById("temp").innerHTML = (`<p>The weather is warm, because it has ${temp} degrees Celsius.</p></br><img src="summer.jpg" alt="warm">`);
-  
+  selTemp.innerHTML = (`<p>The weather is warm, because it has ${temp} degrees Celsius.</p></br><img src="summer.jpg" alt="warm">`);
+
 } else {
-  document.getElementById("temp").innerHTML = (`<p>The weather is hot, because it has ${temp} degrees Celsius.</p></br><img src="desert.jpg" alt="hot">`);
-  document.getElementById("temp").style.backgroundColor = "red";
+  selTemp.innerHTML = (`<p>The weather is hot, because it has ${temp} degrees Celsius.</p></br><img src="desert.jpg" alt="hot">`);
+  selTemp.style.backgroundColor = "red";
 }
 
 // Compare two numbers
@@ -37,22 +38,29 @@ let grade = "";
 let sum = 0;
 let avg = 0;
 
+let selGrades = document.getElementById("grades");
+
 for (let i = 0; i < grades.length; i++) {
   if (grades[i] < 60) {
     grade = "F";
-    console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    // selGrades.innerHTML = students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade;
+    selGrades.innerHTML += `<p>${students[i]}, you have ${grades[i]} points, that means your grade is ${grade}</p><br>`;
   } else if (grades[i] >= 60 && grades[i] < 70) {
     grade = "D";
-    console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    //console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    selGrades.innerHTML += `<p>${students[i]}, you have ${grades[i]} points, that means your grade is ${grade}</p><br>`;
   } else if (grades[i] >= 70 && grades[i] < 80) {
     grade = "C";
-    console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    //console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    selGrades.innerHTML += `<p>${students[i]}, you have ${grades[i]} points, that means your grade is ${grade}</p><br>`;
   } else if (grades[i] >= 80 && grades[i] < 90) {
     grade = "B";
-    console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    //console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    selGrades.innerHTML += `<p>${students[i]}, you have ${grades[i]} points, that means your grade is ${grade}</p><br>`;
   } else {
     grade = "A";
-    console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    //console.log(students[i] + ", you have " + grades[i] + " points, that means your grade is " + grade);
+    selGrades.innerHTML += `<p>${students[i]}, you have ${grades[i]} points, that means your grade is ${grade}</p><br>`;
   }
   sum += grades[i];
   avg = sum / grades.length;
@@ -60,4 +68,21 @@ for (let i = 0; i < grades.length; i++) {
   //console.log(students[i] + " has " + grades[i] + " points");
 }
 
-console.log("Points of all students together: " + sum, "Average Grade: " + avg);
+selGrades.innerHTML += `Points of all students together: ${sum}  --- Average Grade: ${avg}`;
+
+
+//FizzBuzz
+
+let selFizz = document.getElementById("fiZZbuZZ");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 == 0) {
+    selFizz.innerHTML += "FiZZbuZZ" + "<br>";
+  } else if (i % 5 == 0) {
+    selFizz.innerHTML += "BuZZ" + "<br>";
+  } else if (i % 3 == 0) {
+    selFizz.innerHTML += "FiZZ" + "<br>";
+  } else {
+    selFizz.innerHTML += i + "<br>";
+  }
+}
